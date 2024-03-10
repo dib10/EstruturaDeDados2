f79c3a8c07f6798806f753001def04ee41f2b1a2
dib10 <caiodib10@gmail.com>
2024-03-10 18:31:30 -0300

    slide 25

--- 03- Tabela Hash/atividade1/hashTable.c
+++ 03- Tabela Hash/atividade1/hashTable.c
@@ -50,3 +50,10 @@ int chaveDivisao(int chave, int TABLE_SIZE)
 {
     return (chave & 0x7FFFFFFF) % TABLE_SIZE;
 }
+int chaveMultiplicacao(int chave, int TABLE_SIZE)
+{
+    float A = 0.6180339887; // constante: 0 < A < 1
+    float val = chave * A;
+    val = val - (int)val;
+    return (int)(TABLE_SIZE * val);
+}
