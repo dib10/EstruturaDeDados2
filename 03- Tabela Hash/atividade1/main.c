86738580f22445539da436cfe0f83291051fbe93
dib10 <caiodib10@gmail.com>
2024-03-10 18:48:38 -0300

    introduzindo o main

---
+++ 03- Tabela Hash/atividade1/main.c
@@ -0,0 +1,13 @@
+/*
+Implementar o programa hashTable no formato de TAD, com os três arquivos: hashTable.h,
+hashTable.c e main.c. Você poderá incluir mais bibliotecas caso queira (eu acho recomendável),
+para incluir funções auxiliares.
+• Em seu projeto, você desenvolverá todo o arquivo main.c, que então controlará a execução de todo
+o programa da seguinte forma:
+• Um menu apresentará as opções de execução da tabela Hash com inserções sem tratamento de
+colisões, ou inserções com tratamento de colisões. O programa deverá funcionar somente com
+uma das opções, e nunca as duas juntas por motivos óbvios, uma vez que as funções de hashing
+são diferentes. Deverá apresentar ainda a opção de encerramento do programa.
+• Uma vez determinado o passo anterior, um segundo menu deverá apresentar as opções de
+inserção de elementos e busca de elementos, com o questionamento após cada tarefa, se o
+usuário deseja continuar com as operações. Se não desejar, o programa será encerrado.*/
